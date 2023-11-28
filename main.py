461487ef9369502467914ecaff543ff3f3c61945
l01o4 <l01o4@mail.ru>
2023-11-28 22:16:13 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import telebot
+# bot = telebot.TeleBot('6809079629:AAGb6qg4zZrd1Ph6HgXu3ehRS0QBqeT8YlQ')
+bot = telebot.TeleBot('6575827672:AAHczHa7SxtXgUVoiau5zkMmfkEycMaUXAU')
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, 'Здравствуй, котик я кусок мяса. Тебе рассказать о видах мяса?', parse_mode='Markdown')
+@bot.message_handler(commands=['да'])
+def main(message):
+    bot.send_message(message.chat.id, 'Хорошо! Говядина,баранина,свинина,птичье мясо и рыбье мясо', parse_mode='Markdown')
+@bot.message_handler(commands=['нет'])
+def main(message):
+    bot.send_message(message.chat.id, 'Печально! Ну, тогда ешь печеньку!', parse_mode='Markdown')
+bot.infinity_polling()
